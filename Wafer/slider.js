a9eb55013a56c43c3f98f88a922eeefcf35a1072
Elrich D'souza <55625944+ZackNeptune@users.noreply.github.com>
2023-02-13 11:57:30 +0530

    Add files via upload

---
+++ Wafer/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.flickity-gallery').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        freeScroll: true,
+        wrapAround: true,
+    });
+});
+
+// var flkty = new Flickity( '.flickity-gallery', {
+//     // options
+//     cellAlign: 'left',
+//     contain: true,
+//     freeScroll: true,
+//         wrapAround: true,
+//   });
